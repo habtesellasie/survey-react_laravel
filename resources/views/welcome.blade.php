940b35173d4471a5a9652de5414a38d8e553ad0e
habtesellasie <havecodesoft@gmail.com>
2024-03-11 18:25:46 +0300

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+    </head>
+    <body class="bg-slate-300">
+        
+    </body>
+</html>
